319107104a2464fa3a44868cf9fe6ff164c2f370
Cyrus Wickersham <cyruswickersham@Cyruss-MacBook-Air.local>
2023-01-17 20:54:10 -0500

    can now add books

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('wan2read/', views.wan2read_index, name='index')
+    path('wan2read/', views.wan2read_index, name='index'),
+    path('wan2read/<int:book_id>/', views.book_detail, name= 'detail'),
+    path('books/add', views.BookAdd.as_view(), name='add_book')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from django.http import HttpResponse
 from .models import Book
 
@@ -11,3 +12,12 @@ def about(request):
 def wan2read_index(request):
     books = Book.objects.all()
     return render(request, 'books/index.html', {'books': books})
+
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/detail.html', {'book':book})
+
+class BookAdd(CreateView):
+    model = Book
+    fields = '__all__'
+    success_url = '/wan2read'
